ef0cdfbb1b5ce6626815932b3987f278b4d0eb8b
Lew Ayotte <lew@lewayotte.com>
2013-10-22 12:06:34 -0400

    Taggin 1.0.6

--- exchange-addon-mailchimp.php
+++ exchange-addon-mailchimp.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - MailChimp Add-on
- * Version: 1.0.5
+ * Version: 1.0.6
  * Description: Adds the MailChimp addon to iThemes Exchange
  * Plugin URI: http://ithemes.com/exchange/mailchimp/
  * Author: iThemes
