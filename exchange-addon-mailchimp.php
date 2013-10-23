33bd198dbcd40188366be44b455dca18270db219
Lew Ayotte <lew@lewayotte.com>
2013-10-23 08:25:37 -0400

    fixing version number

--- exchange-addon-mailchimp.php
+++ exchange-addon-mailchimp.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - MailChimp Add-on
- * Version: 1.0.6
+ * Version: 1.0.7
  * Description: Adds the MailChimp addon to iThemes Exchange
  * Plugin URI: http://ithemes.com/exchange/mailchimp/
  * Author: iThemes
