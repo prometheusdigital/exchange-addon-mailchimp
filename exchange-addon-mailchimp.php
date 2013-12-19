b8447604b5f4a1a17903620b1336a215fafedbfb
Git User <git@petra.ithemes.com>
2013-12-19 14:59:47 -0600

    1.0.12: updated: lib/updater

--- exchange-addon-mailchimp.php
+++ exchange-addon-mailchimp.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - MailChimp Add-on
- * Version: 1.0.11
+ * Version: 1.0.12
  * Description: Adds the MailChimp addon to iThemes Exchange
  * Plugin URI: http://ithemes.com/exchange/mailchimp/
  * Author: iThemes
