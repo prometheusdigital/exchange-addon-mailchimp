ba52391f6791e590dee4e33350749771c381a70d
AJ Morris <ajmorris2002@gmail.com>
2017-07-25 22:19:53 -0400

    Updating rebranding and licensing

--- exchange-addon-mailchimp.php
+++ exchange-addon-mailchimp.php
@@ -1,18 +1,19 @@
 <?php
 /*
- * Plugin Name: iThemes Exchange - MailChimp Add-on
+ * Plugin Name: ExchangeWP - MailChimp Add-on
  * Version: 2.2.1
- * Description: Adds the MailChimp addon to iThemes Exchange
- * Plugin URI: http://ithemes.com/exchange/mailchimp/
- * Author: iThemes
- * Author URI: http://ithemes.com
- * iThemes Package: exchange-addon-mailchimp
- 
+ * Description: Adds the MailChimp addon to ExchangeWP
+ * Plugin URI: https://exchangewp.com/downloads/mailchimp/
+ * Author: ExchangeWP
+ * Author URI: https://exchangewp.com
+ * ExchangeWP Package: exchange-addon-mailchimp
+
  * Installation:
  * 1. Download and unzip the latest release zip file.
  * 2. If you use the WordPress plugin uploader to install this plugin skip to step 4.
  * 3. Upload the entire plugin directory to your `/wp-content/plugins/` directory.
  * 4. Activate the plugin through the 'Plugins' menu in WordPress Administration.
+ * 5. Add license key to plugin settings page.
  *
 */
 
@@ -26,32 +27,32 @@
 function it_exchange_register_mailchimp_addon() {
 	$versions         = get_option( 'it-exchange-versions', false );
 	$current_version  = empty( $versions['current'] ) ? false: $versions['current'];
-	
+
 	if ( true || version_compare( $current_version, '1.35.0', '>' ) ) {
-		
+
 		$options = array(
 			'name'              => __( 'MailChimp', 'LION' ),
 			'description'       => __( 'Add MailChimp Opt-In Checkbox to user registration form.', 'LION' ),
-			'author'            => 'iThemes',
-			'author_url'        => 'http://ithemes.com/exchange/mailchimp/',
+			'author'            => 'ExchangeWP',
+			'author_url'        => 'https://exchangewp.com/downloads/mailchimp/',
 			'icon'              => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/lib/images/mailchimp50px.png' ),
 			'file'              => dirname( __FILE__ ) . '/init.php',
 			'category'          => 'email',
 			'settings-callback' => 'it_exchange_mailchimp_settings_callback',
 		);
 		it_exchange_register_addon( 'mailchimp', $options );
-	
+
 	} else {
-			
+
 		add_action( 'admin_notices', 'it_exchange_add_mailchimp_nag' );
-		
+
 	}
-	
+
 }
 add_action( 'it_exchange_register_addons', 'it_exchange_register_mailchimp_addon' );
 
 /**
- * Adds the MailChimp nag if not on the correct version of iThemes Exchange
+ * Adds the MailChimp nag if not on the correct version of ExchangeWP
  *
  * @since 1.0.0
  * @return void
@@ -60,7 +61,7 @@ function it_exchange_add_mailchimp_nag() {
 	?>
 	<div id="it-exchange-mailchimp-nag" class="it-exchange-nag">
 		<?php
-		printf( __( 'To use the MailChimp add-on for iThemes Exchange, you must be using iThemes Exchange version 1.35.0 or higher. <a href="%s">Please update now</a>.', 'LION' ), admin_url( 'update-core.php' ) );
+		printf( __( 'To use the MailChimp add-on for ExchangeWP, you must be using ExchangeWP version 1.35.0 or higher. <a href="%s">Please update now</a>.', 'LION' ), admin_url( 'update-core.php' ) );
 		?>
 	</div>
     <?php
@@ -86,8 +87,38 @@ add_action( 'plugins_loaded', 'it_exchange_mailchimp_set_textdomain' );
  * @param object $updater ithemes updater object
  * @return void
 */
-function ithemes_exchange_addon_mailchimp_updater_register( $updater ) { 
+function ithemes_exchange_addon_mailchimp_updater_register( $updater ) {
 	    $updater->register( 'exchange-addon-mailchimp', __FILE__ );
 }
 add_action( 'ithemes_updater_register', 'ithemes_exchange_addon_mailchimp_updater_register' );
-require( dirname( __FILE__ ) . '/lib/updater/load.php' );
+// require( dirname( __FILE__ ) . '/lib/updater/load.php' );
+
+if ( ! class_exists( 'EDD_SL_Plugin_Updater' ) )  {
+ 	require_once 'EDD_SL_Plugin_Updater.php';
+ }
+
+ function exchange_mailchimp_plugin_updater() {
+
+ 	// retrieve our license key from the DB
+ 	// this is going to have to be pulled from a seralized array to get the actual key.
+ 	// $license_key = trim( get_option( 'exchange_mailchimp_license_key' ) );
+ 	$exchangewp_mailchimp_options = get_option( 'it-storage-exchange_addon_mailchimp' );
+ 	$license_key = $exchangewp_mailchimp_options['mailchimp_license'];
+
+ 	// setup the updater
+ 	$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
+ 			'version' 		=> '2.2.1', 				// current version number
+ 			'license' 		=> $license_key, 		// license key (used get_option above to retrieve from DB)
+ 			'item_name' 	=> 'mailchimp', 	  // name of this plugin
+ 			'author' 	  	=> 'ExchangeWP',    // author of this plugin
+ 			'url'       	=> home_url(),
+ 			'wp_override' => true,
+ 			'beta'		  	=> false
+ 		)
+ 	);
+ 	// var_dump($edd_updater);
+ 	// die();
+
+ }
+
+ add_action( 'admin_init', 'exchange_mailchimp_plugin_updater', 0 );
--- lib/required-hooks.php
+++ lib/required-hooks.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * iThemes Exchange MailChimp Add-on
+ * ExchangeWP MailChimp Add-on
  * @package IT_Exchange_Addon_MailChimp
  * @since 1.0.0
 */
 
 /**
- * Adds actions to the plugins page for the iThemes Exchange MailChimp plugin
+ * Adds actions to the plugins page for the ExchangeWP MailChimp plugin
  *
  * @since 1.0.0
  *
  * @param array $meta Existing meta
  * @param string $plugin_file the wp plugin slug (path)
  * @param array $plugin_data the data WP harvested from the plugin header
- * @param string $context 
+ * @param string $context
  * @return array
 */
 function it_exchange_mailchimp_plugin_row_actions( $actions, $plugin_file, $plugin_data, $context ) {
-	
+
 	$actions['setup_addon'] = '<a href="' . get_admin_url( NULL, 'admin.php?page=it-exchange-addons&add-on-settings=mailchimp' ) . '">' . __( 'Setup Add-on', 'LION' ) . '</a>';
-	
+
 	return $actions;
-	
+
 }
 add_filter( 'plugin_action_links_exchange-addon-mailchimp/exchange-addon-mailchimp.php', 'it_exchange_mailchimp_plugin_row_actions', 10, 4 );
 
@@ -40,7 +40,7 @@ add_action( 'admin_enqueue_scripts', 'it_exchange_stripe_addon_admin_enqueue_scr
 
 // adds an email to the mailchimp subscription list
 function it_exchange_sign_up_email_to_mailchimp_list() {
-	
+
 	$settings = it_exchange_get_option( 'addon_mailchimp' );
 
 	if ( empty( $_POST['it-exchange-mailchimp-signup'] ) && ! empty( $settings['mailchimp-optin'] ) ) {
@@ -86,7 +86,7 @@ add_action( 'it_exchange_register_user', 'it_exchange_sign_up_email_to_mailchimp
  * @return array|false
 */
 function it_exchange_addon_mailchimp_init_guest_checkout( $email ) {
-	
+
 	$settings = it_exchange_get_option( 'addon_mailchimp' );
 
 	if ( ! empty( $_POST['it-exchange-mailchimp-signup'] ) || empty( $settings['mailchimp-optin'] ) ) {
@@ -118,9 +118,9 @@ add_action( 'it_exchange_init_guest_checkout', 'it_exchange_addon_mailchimp_init
  * @param array $fields existing fields
  * @return array
 */
-function it_exchange_mailchimp_sign_up_add_field_to_registration_template_parts( $fields ) { 
+function it_exchange_mailchimp_sign_up_add_field_to_registration_template_parts( $fields ) {
 
-    /** 
+    /**
      * We want to add our field right before the save button
      * 1) Find the save button
      * 2) Spice our value in right before the save button
@@ -147,9 +147,9 @@ add_filter( 'it_exchange_get_super_widget_registration_fields_elements', 'it_exc
  * @param array $fields existing fields
  * @return array
 */
-function it_exchange_mailchimp_get_super_widget_guest_checkout_fields_elements( $fields ) { 
+function it_exchange_mailchimp_get_super_widget_guest_checkout_fields_elements( $fields ) {
 
-    /** 
+    /**
      * We want to add our field right before the save button
      * 1) Find the save button
      * 2) Spice our value in right before the save button
@@ -175,9 +175,9 @@ add_filter( 'it_exchange_get_super_widget_guest_checkout_fields_elements', 'it_e
  * @param array $fields existing fields
  * @return array
 */
-function it_exchange_mailchimp_get_super_widget_guest_checkout_actions_elements( $fields ) { 
+function it_exchange_mailchimp_get_super_widget_guest_checkout_actions_elements( $fields ) {
 
-    /** 
+    /**
      * We want to add our field right before the save button
      * 1) Find the save button
      * 2) Spice our value in right before the save button
@@ -203,9 +203,9 @@ add_filter( 'it_exchange_get_content-checkout-guest-checkout-purchase-requiremen
  * @param array $template_names the template part names we're looking for right now.
  * @return array
 */
-function it_exchange_mailchimp_add_template_directory( $template_paths, $template_names ) { 
+function it_exchange_mailchimp_add_template_directory( $template_paths, $template_names ) {
 
-    /** 
+    /**
      * Use the template_names array to target a specific template part you want to add
      * In this example, we're adding the following template part: content-registration/fields/details/mailchimp-signup.php and super-widget-registration/fields/details/mailchimp-signup.php
      * So we're going to only add our templates directory if Exchange is looking for that part.
@@ -213,10 +213,10 @@ function it_exchange_mailchimp_add_template_directory( $template_paths, $templat
     if ( ! in_array( 'content-registration/elements/mailchimp-signup.php', $template_names )
 		&& ! in_array( 'content-checkout/elements/purchase-requirements/guest-checkout/elements/mailchimp-signup.php', $template_names )
 		&& ! in_array( 'super-widget-registration/elements/mailchimp-signup.php', $template_names )
-		&& ! in_array( 'super-widget-guest-checkout/elements/mailchimp-signup.php', $template_names ) ) 
+		&& ! in_array( 'super-widget-guest-checkout/elements/mailchimp-signup.php', $template_names ) )
         return $template_paths;
 
-    /** 
+    /**
      * If we are looking for the mailchimp-signup template part, go ahead and add our add_ons directory to the list
      * No trailing slash
     */
